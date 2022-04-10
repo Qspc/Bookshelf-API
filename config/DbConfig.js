785c9af78579610e74673aa4cb946da462118126
Shibgotalloh Sabilana <shibgotalloh.sabilana@prosa.ai>
2022-04-10 13:43:43 +0700

    deployment

--- config/DbConfig.js
+++ config/DbConfig.js
@@ -1,3 +1,12 @@
-module.exports = {
-    url: "mongodb://localhost:27017/coba"
-}
+const mongoose = require('mongoose')
+require('dotenv').config({ path: '.env' });
+const database = process.env.MONGO_URL || "mongodb://localhost:27017/mangojs"
+
+
+
+mongoose.connect(database, { useNewUrlParser: true })
+mongoose.connection.on("connected",()=>{
+    console.log(`${database} terkoneksi. . .`)
+})
+
+module.exports = {mongoose};
